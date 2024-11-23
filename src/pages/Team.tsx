7b51713911fe55d09d8eb9937b41b5624335584e
TimMindat <timmind@aiesec.net>
2024-11-23 13:07:35 +0200

    Add team pics

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -10,11 +10,19 @@ const teamMembers = [
     github: 'https://github.com/TimMindat/',
     email: 'mailto:temraz@ecosense.dev'
   },
-  { name: 'Abdelrahman Mokhtar', role: 'UI/UX Designer & Flutter Developer' },
+  { name: 'Abdelrahman Mokhtar',
+    role: 'UI/UX Designer & Flutter Developer',
+    image: 'https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-6/259504870_1615494885461229_3940457085574229053_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=vOxpPXoc8-MQ7kNvgHOoq8q&_nc_zt=23&_nc_ht=scontent.fcai19-4.fna&_nc_gid=AvASO6d_i8bya8q3aOwo-6Z&oh=00_AYC__vV8-yOkqLmhbjy1eF-kozjHDjwBEPAG0DyB4rZaWw&oe=67477C0B' },
   { name: 'Kholoud Ahmed', role: 'Kotlin Developer' },
-  { name: 'Mariam Khamees', role: 'Backend Specialist' },
+  { name: 'Mariam Khamees', 
+    role: 'Backend Specialist',
+    image: 'https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-6/295009032_2147215568781554_4718229097804074309_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=XhRzVeyULX0Q7kNvgFyLtbP&_nc_zt=23&_nc_ht=scontent.fcai19-4.fna&_nc_gid=ApaxZGO_tXBPA6wfr9MCD2r&oh=00_AYDgz57h3mrVVavmnS5N1Y1NS5_pejrfxxmT2yUi6AWhuA&oe=674794D3'
+  },
   { name: 'Abdallah Salah', role: 'Hardware Specialist' },
-  { name: 'Hamdy Sayed', role: 'Frontend Developer' }
+  { name: 'Hamdy Sayed',
+    role: 'Frontend Developer',
+    image: 'https://photos.fife.usercontent.google.com/pw/AP1GczO5Atcjp0Km9JRci0QoxGSF3jmJn4zm92Tq4K6SsJEjCECQeT_WnAdnRg=w716-h951-s-no-gm?authuser=0'
+  }
 ];
 
 export function Team() {
